386932163a3dafd9b11943d1797ef503d185228a
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-07-31 13:50:12 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,45 +6,50 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
-    void bfs(int row, int col, vector<vector<char>> &grid, vector<vector<int>> &vis){
-        int rows = grid.size();
-        int cols = grid[0].size();
-        vis[row][col] = 1;
+    void bfs(pair<int, int> node, vector<vector<char>>& grid, vector<vector<int>> &vis){
         queue<pair<int, int>> q;
-        q.push({row, col});
+        q.push(node);
+        int r = node.first;
+        int c = node.second;
+        vis[r][c] = 1;
+        int n = grid.size();
+        int m = grid[0].size();
         
         while(!q.empty()){
-            int currRow = q.front().first;
-            int currCol = q.front().second;
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
             
             for(int delRow = -1; delRow <= 1; delRow++){
                 for(int delCol = -1; delCol <= 1; delCol++){
-                    int nRow = currRow + delRow;
-                    int nCol = currCol + delCol;
-                    
-                    if(nRow >= 0 && nRow < rows && nCol >= 0 && nCol < cols && 
-                    vis[nRow][nCol] == 0 && grid[nRow][nCol] == '1'){
-                        vis[nRow][nCol] = 1;
-                        q.push({nRow, nCol});
+                    int newRow = row + delRow;
+                    int newCol = col + delCol;
+                    if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && grid[newRow][newCol] == '1' && !vis[newRow][newCol]){
+                        q.push({newRow, newCol});
+                        vis[newRow][newCol] = 1;
                     }
                 }
             }
         }
     }
+    
     int numIslands(vector<vector<char>>& grid) {
         int cnt = 0;
-        int rows = grid.size();
-        int cols = grid[0].size();
-        vector<vector<int>> vis(rows, vector<int>(cols, 0));
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(grid[i][j] == '1' && vis[i][j] == 0){
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1' && !vis[i][j]){
                     cnt++;
-                    bfs(i, j, grid, vis);
+                    bfs({i, j}, grid, vis);
                 }
             }
         }
+        
+        
         return cnt;
     }
 };
