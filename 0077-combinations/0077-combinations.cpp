d6730e0a00cb5a2a6f5ad6c123d21ae4b56caf35
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-03 11:10:17 +0530

    Time: 14 ms (88.53%), Space: 18 MB (33.54%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void helper(int i, int n, int k, vector<int> &subset, vector<vector<int>> &ans){
+    void helper(int i, vector<int> &subset, vector<vector<int>> &ans, int n, int k){
         if(k == 0){
             ans.push_back(subset);
             return;
@@ -8,19 +8,24 @@ public:
         if(i > n){
             return;
         }
+        int req = k - subset.size();
+        int left = n - i + 1;
 
+        if(left < req){
+            return;
+        }
+        
         //take
         subset.push_back(i);
-        helper(i + 1, n, k - 1, subset, ans);
-
+        helper(i + 1, subset, ans, n, k - 1);
         //not take
         subset.pop_back();
-        helper(i + 1, n, k, subset, ans);
+        helper(i + 1, subset, ans, n, k);
     }
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
         vector<int> subset;
-        helper(1, n, k, subset, ans);
+        helper(1, subset, ans, n, k);
         return ans;
     }
 };
