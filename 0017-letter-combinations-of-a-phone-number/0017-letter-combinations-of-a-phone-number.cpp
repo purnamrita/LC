11311788c8207f58bcc5081fb41355b6fd617872
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-03 11:48:42 +0530

    Time: 0 ms (100%), Space: 6.5 MB (55.27%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<string> helper(int i, string &digits, map<int, string> &mp){
-        if(i == digits.size()){
+        int n = digits.size();
+        if(i == n){
             return {""};
         }
         vector<string> v = helper(i + 1, digits, mp);
         vector<string> ans;
 
-        for(auto ch : mp[digits[i] - '0']){
+        for(char ch : mp[digits[i] - '0']){
             for(auto it : v){
                 ans.push_back(ch + it);
             }
         }
         return ans;
     }
+
     vector<string> letterCombinations(string digits) {
-        if(digits.size() == 0){
+        if(digits.length() == 0){
             return {};
         }
-
         map<int, string> mp;
         mp[2] = "abc";
         mp[3] = "def";
@@ -28,7 +29,6 @@ public:
         mp[7] = "pqrs";
         mp[8] = "tuv";
         mp[9] = "wxyz";
-
-       return helper(0, digits, mp);
+        return helper(0, digits, mp);
     }
 };
