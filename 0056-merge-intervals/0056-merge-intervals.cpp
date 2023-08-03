f96a8588231d053dba09810958ac5fe84f6932d0
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-03 11:24:00 +0530

    Time: 22 ms (99.66%), Space: 18.9 MB (70.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,14 +4,13 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
-        int n = intervals.size();
 
-        for(int i = 1; i < n; i++){
-            if(intervals[i][0] > ans.back()[1]){
-                ans.push_back(intervals[i]);
+        for(int i = 1; i < intervals.size(); i++){
+            if(intervals[i][0] <= ans.back()[1]){
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
             else{
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+                ans.push_back(intervals[i]);
             }
         }
 
