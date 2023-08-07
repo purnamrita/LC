829690d923b7ff167eda02fcd01e5e54ef688bd2
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-07 13:16:20 +0530

    Time: 3 ms (82.89%), Space: 9.6 MB (19.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,18 +4,41 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        int row = 0;
-        int col = m - 1;
+        int top = 0;
+        int bot = n - 1;
 
-        while(row <= n - 1 && col >= 0){
-            if(matrix[row][col] == target){
-                return true;
+        while(top <= bot){
+            int mid = top + (bot - top) / 2;
+            if(target > matrix[mid][m - 1]){
+                top = mid + 1;
             }
-            else if(matrix[row][col] > target){
-                col--;
+            else if(target < matrix[mid][0]){
+                bot = mid - 1;
             }
             else{
-                row++;
+                break;
+            }
+        }
+
+        if(top > bot){
+            return false;
+        }
+
+        int row = top + (bot - top) / 2;
+
+        int l = 0;
+        int r = m - 1;
+
+        while(l <= r){
+            int m = l + (r - l) / 2;
+            if(matrix[row][m] > target){
+                r = m - 1;
+            }
+            else if(matrix[row][m] < target){
+                l = m + 1;
+            }
+            else{
+                return true;
             }
         }
 
