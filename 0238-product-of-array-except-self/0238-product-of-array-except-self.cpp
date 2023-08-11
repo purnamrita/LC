ba8eb04f31bc8c1b7e09fd6245a3a1779bd56d11
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-11 14:18:17 +0530

    Time: 19 ms (86.87%), Space: 24.6 MB (50.1%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        // [1, 2, 3, 4] pro = 24
-        // 24   12  8  6
-
-        // [1, 2, 3, 4]
-        // prefix
-        // [1, 1, 2, 6]
-        // suffix
-        // [  24  ,12   ,4  ,1]
-
-        // p -> [1, 1, 2, 6]
-        // s -> [24, 12, 4, 1]
-        // ans -?[24, 12, 8, 6]
-
-        // time: O(n + n + n) = O(n)
-        // space: O(n + n) = O(n)
-
         int n = nums.size();
+
         vector<int> prefix(n);
-        vector<int> suffix(n);
         prefix[0] = 1;
         for(int i = 1; i < n; i++){
             prefix[i] = prefix[i - 1] * nums[i - 1];
         }
-        suffix[n - 1] = 1;
-        for(int i = n - 2; i >= 0; i--){
-            suffix[i] = suffix[i + 1] * nums[i + 1];
-        }
-
         vector<int> ans(n);
-        for(int i = 0; i < n; i++){
-            ans[i] = prefix[i] * suffix[i];
+        int suffix = 1;
+        ans[n - 1] = prefix[n - 1] * suffix;
+
+        for(int i = n - 2; i >= 0; i--){
+            suffix *= nums[i + 1];
+            ans[i] = prefix[i] * suffix;
         }
 
         return ans;
