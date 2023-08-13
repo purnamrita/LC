59ee8601f083e1ef0317fe8f105bda5846c3e7dd
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-14 00:51:41 +0530

    Time: 2 ms (50.96%), Space: 10.1 MB (26.34%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,20 +12,14 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == NULL && q == NULL){
-            return true;
-        }
-        else if(p == NULL){
-            return false;
+        if(p == NULL || q == NULL){
+            return (p == q);
         }
         else{
-            if(p != NULL && q != NULL && p -> val != q -> val){
-                return false;
-            }
-            if(p != NULL && q != NULL && isSameTree(p -> left, q -> left) && isSameTree(p -> right, q -> right)){
-                return true;
-            }
+            return (p -> val == q -> val &&
+                    isSameTree(p -> left, q -> left) &&
+                    isSameTree(p -> right, q -> right)
+                    );
         }
-        return false;
     }
 };
