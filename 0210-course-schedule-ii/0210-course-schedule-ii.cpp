daf193ae27c4c18672a82b615ca270d9e31c921e
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-14 16:56:04 +0530

    Time: 16 ms (89.74%), Space: 14.4 MB (26.47%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> topo;
-
         vector<int> adj[numCourses];
 
         for(auto it : prerequisites){
             int a = it[0];
             int b = it[1];
-
             adj[b].push_back(a);
         }
 
-        queue<int> q;
         vector<int> indegree(numCourses);
-
         for(auto v : adj){
-            for(auto it : v){
-                indegree[it]++;
+            for(int node : v){
+                indegree[node]++;
             }
         }
 
-        for(int i = 0; i < numCourses; i++){
+        queue<int> q;
+        for(int i = 0; i < indegree.size(); i++){
             if(indegree[i] == 0){
                 q.push(i);
             }
         }
 
+        vector<int> topo;
         while(!q.empty()){
             int node = q.front();
             q.pop();
             topo.push_back(node);
-
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0){
-                    q.push(it);
+            for(int n : adj[node]){
+                indegree[n]--;
+                if(indegree[n] == 0){
+                    q.push(n);
                 }
             }
         }
 
-        if(topo.size() != numCourses){
-            return {};
+        if(topo.size() == numCourses){
+            return topo;
         }
-        return topo;
+        return {};
     }
 };
