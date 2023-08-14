0c6355afa2828a9d839394e84daac1bf50334cac
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-14 16:48:14 +0530

    Time: 20 ms (81.12%), Space: 14.1 MB (36.64%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -3,39 +3,37 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> adj[numCourses];
 
-        for(auto it: prerequisites){
-            int b = it[0];
-            int a  = it[1];
-
+        for(auto it : prerequisites){
+            int a = it[0];
+            int b = it[1];
             adj[b].push_back(a);
         }
 
         vector<int> indegree(numCourses);
 
         for(auto v : adj){
-            for(auto it : v){
-                indegree[it]++;
+            for(auto node : v){
+                indegree[node]++;
             }
         }
 
         queue<int> q;
-        vector<int> topo;
-
-        for(int i = 0; i < numCourses; i++){
+        for(int i = 0; i < indegree.size(); i++){
             if(indegree[i] == 0){
                 q.push(i);
             }
         }
 
+        vector<int> topo;
+
         while(!q.empty()){
             int node = q.front();
             q.pop();
             topo.push_back(node);
-
-            for(auto it : adj[node]){
-                indegree[it]--;
-                if(indegree[it] == 0){
-                    q.push(it);
+            for(auto n : adj[node]){
+                indegree[n]--;
+                if(indegree[n] == 0){
+                    q.push(n);
                 }
             }
         }
