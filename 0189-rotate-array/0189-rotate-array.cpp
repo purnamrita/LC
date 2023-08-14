01060acabc6dff3510e22e3967448a4662ba19ef
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-14 14:38:25 +0530

    Time: 31 ms (61.27%), Space: 24.9 MB (87.21%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -8,10 +8,10 @@ public:
         }
     }
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        int d = (n - k % n) % n;
-        reverse(nums, 0, d - 1);
-        reverse(nums, d, n - 1);
-        reverse(nums, 0, n - 1);
+       int n = nums.size();
+       k = k % n;
+       reverse(nums, n - k, n - 1);
+       reverse(nums, 0, n - k - 1);
+       reverse(nums, 0, n - 1);
     }
 };
