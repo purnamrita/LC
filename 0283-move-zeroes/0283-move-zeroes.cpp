02151c7fb7e887778167cdc35ca6100a86a7c917
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-14 14:58:32 +0530

    Time: 10 ms (98.96%), Space: 19.1 MB (71.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int zero = -1;
-
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 0){
-                zero = i;
-                break;
-            }
-        }
-
-        if(zero != -1){
-            for(int i = zero + 1; i < n; i++){
-                if(nums[i] != 0){
-                    swap(nums[zero], nums[i]);
-                    zero++;
-                }
+        int left = 0;
+        
+        for(int right = 0; right < nums.size(); right++){
+            if(nums[right] != 0){
+                swap(nums[left], nums[right]);
+                left++;
             }
         }
     }
