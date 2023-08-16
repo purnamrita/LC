3c8e4ecc2ab5b9021ecbbd05d391e958ec3ccbe6
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-17 01:07:00 +0530

    Time: 16 ms (98.77%), Space: 30.9 MB (13.36%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,31 +11,31 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* root) {
+    int leftHeight(TreeNode* root){
         int cnt = 0;
-        if(root == NULL){
-            return cnt;
+        while(root){
+            cnt++;
+            root = root -> left;
         }
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while(!q.empty()){
-            int n = q.size();
-            cnt += n;
-
-            for(int i = 0; i < n; i++){
-                TreeNode* curr = q.front();
-                q.pop();
-                if(curr -> left){
-                    q.push(curr -> left);
-                }
-                if(curr -> right){
-                    q.push(curr -> right);
-                }
-            }
-        }
-
         return cnt;
+    }
+    int rightHeight(TreeNode* root){
+        int cnt = 0;
+        while(root){
+            cnt++;
+            root = root -> right;
+        }
+        return cnt;
+    }
+    int countNodes(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+        if(lh == rh){
+            return pow(2, lh) - 1;
+        }
+        return 1 + countNodes(root -> left) + countNodes(root -> right);
     }
 };
