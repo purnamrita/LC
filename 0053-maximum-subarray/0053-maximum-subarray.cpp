2b73685277183c5237141bc76e15b119e4cb17d4
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-20 17:33:43 +0530

    Time: 76 ms (93.58%), Space: 67.7 MB (82.25%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int ans = INT_MIN;
-        int curr = 0;
+	int curr = 0;
+	for(int i = 0; i < nums.size(); i++){
+		curr += nums[i];
+		ans = max(ans, curr);
+		if(curr < 0){
+			curr = 0;
+}
+}
+	return ans;
 
-        for(int i = 0; i < nums.size(); i++){
-            curr += nums[i];
-            ans = max(curr, ans);
-            if(curr < 0){
-                curr = 0;
-            }
-        }
-
-        return ans;
     }
+
+    // Time: O(n)
+    // space: O(1)
+    //Kadane's algo
 };
