6bdb99a0f9ef132ebbca260d538a63e80036f0df
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-08-20 22:35:40 +0530

    Time: 29 ms (85.14%), Space: 19 MB (44.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -6,7 +6,7 @@ public:
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
 
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i < n; i++){
             if(intervals[i][0] <= ans.back()[1]){
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
