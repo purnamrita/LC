581f38a809607a22455f1d1ebc749d8bc72eed89
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-19 22:25:02 +0530

    Time: 3 ms (47.11%), Space: 6.6 MB (39.46%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,48 +1,20 @@
 class Solution {
 public:
+    string stringRemove(string str){
+        string ans;
+        for(char ch : str){
+            if(ch == '#'){
+                if(!ans.empty()){
+                    ans.pop_back();
+                }
+            }
+            else{
+                ans += ch;
+            }
+        }
+        return ans;
+    }
     bool backspaceCompare(string s, string t) {
-        stack<char> st;
-
-        for(int i = 0; i < s.length(); i++){
-            if(s[i] != '#'){
-                st.push(s[i]);
-            }
-            else{
-                if(st.empty() == false){
-                    st.pop();
-                }
-            }
-        }
-        string sr;
-        while(!st.empty()){
-            sr += st.top();
-            st.pop();
-        }
-
-        for(int i = 0; i < t.length(); i++){
-            if(t[i] != '#'){
-                st.push(t[i]);
-            }
-            else{
-                if(st.empty() == false){
-                    st.pop();
-                }
-            }
-        }
-        string str;
-        while(!st.empty()){
-            str += st.top();
-            st.pop();
-        }
-
-        if(sr.length() != str.length()){
-            return false;
-        }
-        for(int i = 0; i < sr.length(); i++){
-            if(sr[i] != str[i]){
-                return false;
-            }
-        }
-        return true;
+        return stringRemove(s) == stringRemove(t);
     }
 };
