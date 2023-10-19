ea53206514a09c702cbac28e2e390e5b2925e98f
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-19 16:43:37 +0530

    Time: 0 ms (100%), Space: 9.3 MB (31.85%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int helper(int r, int c, vector<vector<int>>& triangle, vector<vector<int>>& dp){
-        int n = triangle.size();
-        if(r == n - 1){
-            return dp[r][c] = triangle[r][c];
-        }
-
-        if(dp[r][c] != -1){
-            return dp[r][c];
-        }
-
-        int left = helper(r + 1, c, triangle, dp);
-        int right = helper(r + 1, c + 1, triangle, dp);
-
-        return dp[r][c] = triangle[r][c] + min(left, right);
-    }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        return helper(0, 0, triangle, dp);
+
+        for(int r = n - 1; r >= 0; r--){
+            for(int c = 0; c <= r; c++){
+                if(r == n - 1){
+                    dp[r][c] = triangle[r][c];
+                }
+                else{
+                    dp[r][c] = triangle[r][c] + min(dp[r + 1][c], dp[r + 1][c + 1]);
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
