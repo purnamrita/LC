04e493af697b9da652aaa305e3c2462ac081b517
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-21 23:54:42 +0530

    Time: 38 ms (25.02%), Space: 18.6 MB (51.65%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int helper(int idx, int amount, vector<int>& coins, vector<vector<int>> &dp){
-        if(idx == 0){
-            if(amount % coins[0] == 0){
-                return 1;
-            }
-            return 0;
-        }
-
-        if(dp[idx][amount] != -1){
-            return dp[idx][amount];
-        }
-
-        int notTake = helper(idx - 1, amount, coins, dp);
-        int take = 0;
-        if(coins[idx] <= amount){
-            take = helper(idx, amount - coins[idx], coins, dp);
-        }
-        return dp[idx][amount] = take + notTake;
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        return helper(n - 1, amount, coins, dp);
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+        for(int amt = 0; amt <= amount; amt++){
+            dp[0][amt] = (amt % coins[0] == 0);
+        }
+
+        for(int row = 1; row < n; row++){
+            for(int amt = 0; amt <= amount; amt++){ 
+                int notTake = dp[row - 1][amt];
+                int take = 0;
+                if(coins[row] <= amt){
+                    take = dp[row][amt - coins[row]];
+                }
+                dp[row][amt] = take + notTake;
+            }
+        }
+
+        return dp[n - 1][amount];
     }
 };
