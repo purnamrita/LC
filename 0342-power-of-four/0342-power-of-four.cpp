c065b80ba20f4c6b1f3bf04b43ecac8babdf5297
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-24 00:46:17 +0530

    Time: 0 ms (100%), Space: 6.4 MB (13.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == 1){
-            return true;
-        }
-        if(n <= 0 || n % 4){
-            return false;
-        }
-        return isPowerOfFour(n / 4);
+        double number = n;
+        double base = 4.0;
+        double res = log(number) / log(base);
+
+        return n > 0 && fmod(res, 1.0) == 0.0;
     }
 };
