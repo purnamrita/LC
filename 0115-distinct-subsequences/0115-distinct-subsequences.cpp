1a5c419b7a9c1ba754289711ebe99e8cc0e7893a
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-23 22:26:35 +0530

    Time: 20 ms (90.13%), Space: 6.8 MB (86.58%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -3,26 +3,21 @@ public:
     int numDistinct(string s, string t) {
         int slen = s.length();
         int tlen = t.length();
-        vector<vector<double>> dp(slen + 1, vector<double>(tlen + 1, -1));
         
-        for(int i = 0; i <= slen; i++){
-            dp[i][0] = 1;
-        }
-        for(int j = 1; j <= tlen; j++){
-            dp[0][j] = 0;
-        }
+        vector<double> prev(tlen + 1, 0);
+        prev[0] = 1;
 
         for(int i = 1; i <= slen; i++){
-            for(int j = 1; j <= tlen; j++){
+            for(int j = tlen; j >= 1; j--){
                 if(s[i - 1] == t[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                    prev[j] = prev[j - 1] + prev[j];
                 }
                 else{
-                    dp[i][j] = dp[i - 1][j];
+                    prev[j] = prev[j];
                 }
             }
         }
 
-        return (int)dp[slen][tlen];
+        return (int)prev[tlen];
     }
 };
