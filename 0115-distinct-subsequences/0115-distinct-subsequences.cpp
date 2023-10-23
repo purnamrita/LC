cbd55563fafc3adf1beab9a6c8a4377285b72fc5
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-23 22:12:29 +0530

    Time: 44 ms (28.2%), Space: 30.4 MB (32.36%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    int helper(string &s, string &t, int idx1, int idx2, vector<vector<int>> &dp){
-        if(idx2 < 0){
-            return 1;
-        }
-        if(idx1 < 0){
-            return 0;
-        }
-        if(dp[idx1][idx2] != -1){
-            return dp[idx1][idx2];
-        }
-        if(s[idx1] == t[idx2]){
-            return dp[idx1][idx2]  = helper(s, t, idx1 - 1, idx2 - 1, dp) + helper(s, t, idx1 - 1, idx2, dp);
-        }
-        else{
-            return dp[idx1][idx2] = helper(s, t, idx1 - 1, idx2, dp);
-        }
-    }
     int numDistinct(string s, string t) {
         int slen = s.length();
         int tlen = t.length();
-        vector<vector<int>> dp(slen, vector<int>(tlen, -1));
-        return helper(s, t, slen - 1, tlen - 1, dp);
+        vector<vector<long long>> dp(slen + 1, vector<long long>(tlen + 1, -1));
+        
+        for(int i = 0; i <= slen; i++){
+            dp[i][0] = 1;
+        }
+        for(int j = 1; j <= tlen; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1; i <= slen; i++){
+            for(int j = 1; j <= tlen; j++){
+                if(s[i - 1] == t[j - 1]){
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) > INT_MAX ? INT_MAX : (dp[i - 1][j - 1] + dp[i - 1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        if(dp[slen][tlen] == INT_MAX){
+            return -1;
+        }
+        return dp[slen][tlen];
     }
 };
