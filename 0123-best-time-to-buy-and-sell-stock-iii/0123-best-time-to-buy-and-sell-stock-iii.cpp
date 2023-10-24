2cb1747807f86bb0e862213bf56d90d90a2ea2e4
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-24 22:32:56 +0530

    Time: 283 ms (55.95%), Space: 132.9 MB (57.71%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
+    int helper(int idx, int trans, vector<int>& prices, vector<vector<int>> &dp){
+        int n = prices.size();
+        if(idx == n || trans == 4){
+            return 0;
+        }
+        if(dp[idx][trans] != -1){
+            return dp[idx][trans];
+        }
+        if(trans % 2 == 0){
+            //buy
+            return dp[idx][trans] = max(
+                -prices[idx] + helper(idx + 1, trans + 1, prices, dp),
+                helper(idx + 1, trans, prices, dp)
+            );
+        }
+        else{
+            //sell
+            return dp[idx][trans] = max(
+                prices[idx] + helper(idx + 1, trans + 1, prices, dp),
+                helper(idx + 1, trans, prices, dp)
+            );
+        }
+    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int cap = 2;
-
-        vector<vector<int>> after(2, vector<int>(cap + 1, 0));
-        
-        for(int idx = n - 1; idx >= 0; idx--){
-            vector<vector<int>> curr(2, vector<int>(cap + 1, 0));
-            for(int buy = 0; buy < 2; buy++){
-                for(int cap = 1; cap <= 2; cap++){
-                    if(buy == 1){
-                        curr[buy][cap] = max(
-                            -prices[idx] + after[0][cap],
-                            after[1][cap]
-                        );
-                    }
-                    else{
-                        curr[buy][cap] = max(
-                            prices[idx] + after[1][cap - 1],
-                            after[0][cap]
-                        );
-                    }
-                }
-            }
-            after = curr;
-        }
-
-        return after[1][2];
+        vector<vector<int>> dp(n + 1, vector<int>(4, -1));
+        return helper(0, 0, prices, dp);
     }
 };
