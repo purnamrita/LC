19ed847a9128374430132f5002732a785995db9f
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-24 16:54:02 +0530

    Time: 3 ms (90.2%), Space: 15.5 MB (22.25%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
-    int helper(int idx, int buy, vector<int>& prices, vector<vector<int>> &dp){
-        int n = prices.size();
-        if(idx == n){
-            return 0;
-        }
-        if(dp[idx][buy] != -1){
-            return dp[idx][buy];
-        }
-        if(buy){
-            return dp[idx][buy] = max(
-                -prices[idx] + helper(idx + 1, 0, prices, dp),
-                helper(idx + 1, 1, prices, dp)
-            );
-        }
-        else{
-            return dp[idx][buy] = max(
-                prices[idx] + helper(idx + 1, 1, prices, dp),
-                helper(idx + 1, 0, prices, dp)
-            );
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return helper(0, 1, prices, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        dp[n][0] = dp[n][1] = 0;
+
+        for(int i = n - 1; i >= 0; i--){
+            dp[i][1] = max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
+            dp[i][0] = max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
+        }
+        return dp[0][1];
     }
 };
