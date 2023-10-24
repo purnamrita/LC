2a0556d5f8550280b1329ac9719d05d2cdaecec9
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-25 00:38:47 +0530

    Time: 10 ms (76.26%), Space: 13.9 MB (60.58%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
-                int n = prices.size();
-                vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
+        int n = prices.size();
+        vector<int> after(2 * k + 1, 0);
 
-                vector<vector<int>> after(2, vector<int>(k + 1, 0));
-                
-                for(int idx = n - 1; idx >= 0; idx--){
-                    vector<vector<int>> curr(2, vector<int>(k + 1, 0));
-                    for(int buy = 0; buy < 2; buy++){
-                        for(int cap = 1; cap <= k; cap++){
-                            if(buy == 1){
-                                curr[buy][cap] = max(
-                                    -prices[idx] + after[0][cap],
-                                    after[1][cap]
-                                );
-                            }
-                            else{
-                                curr[buy][cap] = max(
-                                    prices[idx] + after[1][cap - 1],
-                                    after[0][cap]
-                                );
-                            }
-                        }
-                    }
-                    after = curr;
+        for(int idx = n - 1; idx >= 0; idx--){
+            vector<int> curr(2*k + 1, 0);
+            for(int trans = 2*k - 1; trans >= 0; trans--){
+                if(trans % 2 == 0){
+                    //buy
+                    curr[trans] = max(
+                        -prices[idx] + after[trans + 1],
+                        after[trans]
+                    );
                 }
-
-                return after[1][k];
+                else{
+                    //sell
+                    curr[trans] = max(
+                        prices[idx] + after[trans + 1],
+                        after[trans]
+                    );
+                }
             }
+            after = curr;
+        }
+
+        return after[0];
+    
+    }
 };
