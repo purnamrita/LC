b2a524c81d2b64b53a0222266f3e028d254023c1
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-25 21:55:50 +0530

    Time: 2 ms (81.58%), Space: 12.2 MB (10.74%) - LeetHub

--- 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
+    int firstOcc(vector<int> &arr, int target){
+        int n = arr.size();
+        int idx = lower_bound(arr.begin(), arr.end(), target) - arr.begin();
+        if(idx != n && arr[idx] == target){
+            return idx;
+        }
+        return -1;
+    }
+    int lastOcc(vector<int> &arr, int target){
+        int n = arr.size();
+        int idx = upper_bound(arr.begin(), arr.end(), target) - arr.begin();
+        if(idx != 0 && arr[idx - 1] == target){
+            return idx - 1;
+        }
+        return -1;
+    }
     vector<int> targetIndices(vector<int>& nums, int target) {
         vector<int> ans;
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < n; i++){
-            if(nums[i] == target){
-                ans.push_back(i);
-            }
+        int firstIdx = firstOcc(nums, target);
+        int lastIdx = lastOcc(nums, target);
+        if(firstIdx == -1){
+            return ans;
+        }
+        for(int i = firstIdx; i <= lastIdx; i++){
+            ans.push_back(i);
         }
         return ans;
     }
