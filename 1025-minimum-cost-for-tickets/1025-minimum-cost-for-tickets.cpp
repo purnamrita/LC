11493dfc5348bb70f9e24295ca451d323c543bc1
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-26 04:04:09 +0530

    Time: 5 ms (35.02%), Space: 9.9 MB (62.12%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    int helper(int idx, vector<int>& days, vector<int>& costs, vector<int> &dp){
-        int n = days.size();
-        if(idx >= n){
-            return 0;
-        }
-        if(dp[idx] != -1){
-            return dp[idx];
-        }
-        // 1 day
-        int op1 = costs[0] + helper(idx + 1, days, costs, dp);
-
-        //7 days
-        int i;
-        for(i = idx; i < n && days[i] < days[idx] + 7; i++);
-        int op2 = costs[1] + helper(i, days, costs, dp);
-
-        //30 days
-        for(i = idx; i < n && days[i] < days[idx] + 30; i++);
-        int op3 = costs[2] + helper(i, days, costs, dp);
-
-        return dp[idx] = min(op1, min(op2, op3));
-
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<int> dp(n + 1, -1);
-        return helper(0, days, costs, dp);
+        vector<int> dp(n + 1, 0);
+        
+        for(int idx = n - 1; idx >= 0; idx--){
+            // 1 day
+            int op1 = costs[0] + dp[idx + 1];
+
+            //7 days
+            int i;
+            for(i = idx; i < n && days[i] < days[idx] + 7; i++);
+            int op2 = costs[1] + dp[i];
+
+            //30 days
+            for(i = idx; i < n && days[i] < days[idx] + 30; i++);
+            int op3 = costs[2] + dp[i];
+
+            dp[idx] = min(op1, min(op2, op3));
+        }
+
+        return dp[0];
     }
 };
