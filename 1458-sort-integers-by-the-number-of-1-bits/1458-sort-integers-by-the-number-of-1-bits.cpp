abdddc30a139ca8acf6f70231e1a44da60dbac6b
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-10-30 06:09:31 +0530

    Time: 21 ms (7.42%), Space: 10.5 MB (66.29%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int count1Bits(int n){
+    static int count1Bits(int n){
         int ans = 0;
         for(int i = 0; i < 32; i++){
             if((n & (1 << i)) != 0){
@@ -10,22 +10,19 @@ public:
         return ans;
     }
 
+    static bool comparator(int num1, int num2){
+        int cnt11 = count1Bits(num1);
+        int cnt12 = count1Bits(num2);
+
+        if(cnt11 == cnt12){
+            return (num1 < num2);
+        }
+        return (cnt11 < cnt12);
+    }
+
     vector<int> sortByBits(vector<int>& arr) {
-        vector<int> ans;
-        map<int, vector<int>> mp;
-        for(int num : arr){
-            int cnt1 = count1Bits(num);
-            mp[cnt1].push_back(num);
-        }
+        sort(arr.begin(), arr.end(), comparator);
 
-        for(auto it : mp){
-            vector<int> temp = it.second;
-            sort(temp.begin(), temp.end());
-            for(int num : temp){
-                ans.push_back(num);
-            }
-        }
-
-        return ans;
+        return arr;
     }
 };
