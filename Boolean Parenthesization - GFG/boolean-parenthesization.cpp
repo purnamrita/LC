f5f487deda33780634ac3348fa633a38aeeb5a93
Purnamrita Bhattacharjee <purnamrita22@gmail.com>
2023-11-11 17:08:17 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -11,64 +11,61 @@ class Solution{
 private:
     int mod = 1003;
 public:
-    int helper(int i, int j, string &exp, bool isTrue, vector<vector<vector<int>>> &dp){
-    if(i > j){
-        return 0;
-    }
-
-    if(dp[i][j][isTrue] != -1){
-        return dp[i][j][isTrue];
-    }
-
-    if(i == j){
-        if(isTrue == 1){
-            return (exp[i] == 'T') ? 1 : 0;
-        }
-        else{
-            return (exp[i] == 'F') ? 1 : 0;
-        }
-    }
-
-    int ways = 0;
-
-    for(int idx = i + 1; idx < j; idx+=2){
-        int lT = helper(i, idx - 1, exp, 1, dp);
-        int lF = helper(i, idx - 1, exp, 0, dp);
-        int rT = helper(idx + 1, j, exp, 1, dp);
-        int rF = helper(idx + 1, j, exp, 0, dp);
-
-        if(exp[idx] == '&'){
-            if(isTrue == 1){
-                ways = (ways % mod) + ((lT * rT) % mod);
-            }
-            else{
-                ways = (ways % mod) + ((lT * rF) % mod) + ((lF * rT) % mod) + ((lF * rF) % mod);
-            }
-        }
-        else if(exp[idx] == '|'){
-            if(isTrue == 1){
-                ways = (ways % mod) + ((lT * rT) % mod) + ((lT * rF) % mod) + ((lF * rT) % mod);
-            }
-            else{
-                ways = (ways % mod) + ((lF * rF) % mod);
-            }
-        }
-        else{
-            if(isTrue == 1){
-                ways = (ways % mod) + ((lT * rF) % mod) + ((lF * rT) % mod);
-            }
-            else{
-                ways = (ways % mod) + ((lT * rT) % mod) + ((lF * rF) % mod);
-            }
-        }
-    }
-
-    return dp[i][j][isTrue] = (ways % mod);
-}
 
     int countWays(int n, string exp){
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(2, -1)));
-        return helper(0, n - 1, exp, 1, dp);
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(n + 1, vector<int>(2, -1)));
+        
+        for(int i = 0; i < n; i++){
+            dp[i][i][0] = (exp[i] == 'F');
+            dp[i][i][1] = (exp[i] == 'T');
+        }
+        
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = 0; j <= n - 1; j++){
+                for(int isTrue = 0; isTrue <= 1; isTrue++){
+                    if(i == j){
+                        continue;
+                    }
+                    int ways = 0;
+                
+                    for(int idx = i + 1; idx < j; idx+=2){
+                        int lT = dp[i][idx - 1][1];
+                        int lF = dp[i][idx - 1][0];
+                        int rT = dp[idx + 1][j][1];
+                        int rF = dp[idx + 1][j][0];
+                
+                        if(exp[idx] == '&'){
+                            if(isTrue == 1){
+                                ways = (ways % mod) + ((lT * rT) % mod);
+                            }
+                            else{
+                                ways = (ways % mod) + ((lT * rF) % mod) + ((lF * rT) % mod) + ((lF * rF) % mod);
+                            }
+                        }
+                        else if(exp[idx] == '|'){
+                            if(isTrue == 1){
+                                ways = (ways % mod) + ((lT * rT) % mod) + ((lT * rF) % mod) + ((lF * rT) % mod);
+                            }
+                            else{
+                                ways = (ways % mod) + ((lF * rF) % mod);
+                            }
+                        }
+                        else{
+                            if(isTrue == 1){
+                                ways = (ways % mod) + ((lT * rF) % mod) + ((lF * rT) % mod);
+                            }
+                            else{
+                                ways = (ways % mod) + ((lT * rT) % mod) + ((lF * rF) % mod);
+                            }
+                        }
+                    }
+
+                    dp[i][j][isTrue] = (ways % mod);
+                }
+            }
+        }
+        
+        return dp[0][n - 1][1];
     }
 };
 
